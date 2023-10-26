2ed5c8c105188c2d4d2e463a5778b9df73f171cb
BossTeemo <ab3433@naver.com>
2023-10-26 20:07:47 +0900

    error_fixed_version_1

--- app/src/main/java/com/example/idealworldcup/ConfirmationFragment.java
+++ app/src/main/java/com/example/idealworldcup/ConfirmationFragment.java
@@ -13,21 +13,23 @@ import androidx.fragment.app.Fragment;
 
 public class ConfirmationFragment extends Fragment {
 
-    private TextView textViewSelectedJob;
-    private Button buttonConfirm;
+    private TextView textViewSelectedJobName;
+    private TextView textView_selectedJobDescription;
+    private Button buttonNext;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_confirmation, container, false);
 
-        textViewSelectedJob = view.findViewById(R.id.textView_selectedJob);
-        buttonConfirm = view.findViewById(R.id.button_confirm);
+        textViewSelectedJobName = view.findViewById(R.id.textView_selectedJobName);
+        textView_selectedJobDescription = view.findViewById(R.id.textView_selectedJobDescription);
+        buttonNext = view.findViewById(R.id.button_next);
 
         // 여기서 선택한 직업을 표시할 수 있습니다.
         // 예: textViewSelectedJob.setText(selectedJobName);
 
-        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+        buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 사용자가 선택을 확인했을 때의 처리를 여기에 작성합니다.
@@ -39,9 +41,12 @@ public class ConfirmationFragment extends Fragment {
     }
 
     // 선택한 직업을 설정하는 메서드
-    public void setSelectedJob(String jobName) {
-        if (textViewSelectedJob != null) {
-            textViewSelectedJob.setText(jobName);
+    public void setSelectedJob(String jobName, String jobDescription) {
+        if (textView_selectedJobDescription != null) {
+            textViewSelectedJobName.setText(jobName);
+        }
+        if (textView_selectedJobDescription != null){
+            textView_selectedJobDescription.setText(jobDescription);
         }
     }
 }
--- app/src/main/java/com/example/idealworldcup/FinalResultFragment.java
+++ app/src/main/java/com/example/idealworldcup/FinalResultFragment.java
@@ -12,30 +12,34 @@ import androidx.fragment.app.Fragment;
 
 public class FinalResultFragment extends Fragment {
 
-    private TextView textViewFinalJob;
-    private TextView textViewSubjects;
+    private TextView textViewFinalJobName;
+    private TextView textViewFinalJobDescription;
+    private TextView textViewFinalSubjects;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_final_result, container, false);
 
-        textViewFinalJob = view.findViewById(R.id.textView_finalJob);
-        textViewSubjects = view.findViewById(R.id.textView_subjects);
+        textViewFinalJobName = view.findViewById(R.id.textView_finalJobName);
+        textViewFinalJobDescription = view.findViewById(R.id.textView_finalJobDescription);
+        textViewFinalSubjects = view.findViewById(R.id.textView_finalSubjects);
 
         // 예시: 선택된 직업과 관련 과목을 표시
         // 이 부분은 실제 로직에 따라 수정되어야 합니다.
-        // textViewFinalJob.setText(selectedFinalJob);
-        // textViewSubjects.setText(relatedSubjects);
+        // textViewFinalJobName.setText(selectedFinalJobName);
+        // textViewFinalJobDescription.setText(selectedFinalJobDescription);
+        // textViewFinalSubjects.setText(relatedSubjects);
 
         return view;
     }
 
     // 최종 결과를 설정하는 메서드
     public void setFinalResult(JobData jobData) {
-        if (textViewFinalJob != null && textViewSubjects != null) {
-            textViewFinalJob.setText(jobData.getJobName());
-            textViewSubjects.setText(String.join(", ", jobData.getSubjects()));
+        if (textViewFinalJobName != null && textViewFinalSubjects != null) {
+            textViewFinalJobName.setText(jobData.getJobName());
+            textViewFinalJobDescription.setText(jobData.getJobDescription());
+            textViewFinalSubjects.setText(String.join(",", jobData.getSubjects()));
         }
     }
 }
